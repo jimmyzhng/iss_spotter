0f25562f1429ba3e49acb2ddde1fb8affb458da2
Jimmy <jimmyzhang1@hotmail.com>
2022-11-25 00:50:16 -0800

    will now introduce promises

--- iss.js
+++ iss.js
@@ -1,15 +1,13 @@
 const request = require("request");
 
 const fetchMyIP = function(callback) {
-  // use request to fetch IP address from JSON API
+
   request('https://api.ipify.org', (error, response, body) => {
-    // console.log(response);
 
     if (error) {
       return callback(error, null);
     }
 
-    // If not 200 status, we assume it is a server error
     if (response.statusCode !== 200) {
       const msg = `Status code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
